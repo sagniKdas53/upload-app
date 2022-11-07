ca7763e1c52c0dc545ba19487b212307cf2cfe42
Sagnik Das <mailsagnikdas53@gmail.com>
2022-11-07 16:28:53 +0530

    idk why this in't working

--- index.js
+++ index.js
@@ -2,35 +2,54 @@ const http = require('http');
 const busboy = require('busboy');
 const fs = require('fs');
 const path = require('path');
+var port = process.argv[2] || 8888;
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(`
-      <form action="/upload" enctype="multipart/form-data" method="post">
-        <input type="file" name="someCoolFiles"><br>
-        <button>Upload</button>
-      </form>
-    `);
-    } else if (req.url === '/upload') {
+        res.write(fs.readFileSync(__dirname + '/index.html'));
+        res.end();
+    } else if (req.url === '/files') {
+        let saved = '';
         let filename = '';
+       
         const bb = busboy({ headers: req.headers });
         bb.on('file', (name, file, info) => {
             filename = info.filename;
+            console.log("Saved: " + filename);
+            saved += "," + filename
             const saveTo = path.join(__dirname + "/recieved", filename);
             file.pipe(fs.createWriteStream(saveTo));
         });
         bb.on('close', () => {
             res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end(`upload success: ${filename}`);
+            res.end(`upload success: ${saved}`);
         });
         req.pipe(bb);
-    } else {
+    } else if (req.url === '/folder') {
+        let saved = '';
+        let filename = '';
+ 
+        const bb = busboy({ headers: req.headers });
+        bb.on('file', (name, file, info) => {
+            filename = info.filename;
+            console.log("Saved: " + filename);
+            saved += "," + filename
+            const saveTo = path.join(__dirname + "/recieved", filename);
+            file.pipe(fs.createWriteStream(saveTo));
+        });
+        bb.on('close', () => {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(`upload success: ${saved}`);
+        });
+        req.pipe(bb);
+    }
+    else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('404');
     }
 });
 
-server.listen(4001, () => {
-    console.log('Server listening on http://localhost:4000 ...');
+server.listen(port, () => {
+    console.log('Server listening on ' + port);
 });
