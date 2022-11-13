1ec34f9c136c8c617041ee8873a596323a456f57
Sagnik Das <mailsagnikdas53@gmail.com>
2022-11-13 20:34:08 +0530

    doesn't work for folder upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const busboy = require('busboy');
 const fs = require('fs');
 const path = require('path');
 const cliProgress = require('cli-progress');
+const colors = require('ansi-colors');
 var port = process.argv[2] || 8888;
 
 var server = http.createServer((req, res) => {
@@ -23,7 +24,7 @@ var server = http.createServer((req, res) => {
         io.on('connection', function (socket) {
             socket.emit('init', { message: "Connected", id: socket.id });
 
-            socket.on('acknowledge', console.log);  
+            //socket.on('acknowledge', console.log);  
             // remove this as this causes the progress bar to break
         });
         io.emit('progress', { progress: 0 });
@@ -65,22 +66,59 @@ var server = http.createServer((req, res) => {
         });
         req.pipe(bb);
     } else if (req.url === '/folder') {
+        file_size = req.headers['content-length'] / (1024 ^ 2);
+        console.log("req.headers['content-length'] in MB: " + file_size / 1024);
+        var total = 0;
         const bb = busboy({ headers: req.headers, preservePath: true });
+        const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+        bar.start(100, 0);
+        var io = require('socket.io')(server);
+        io.on('connection', function (socket) {
+            socket.emit('init', { message: "Connected", id: socket.id });
+            socket.on('acknowledge', console.log);  
+            // remove this as this causes the progress bar to break
+        });
+        io.emit('progress', { progress: 0 });
         bb.on('file', (name, file, info) => {
             filename = decodeURIComponent(escape(info.filename));
             dirpath = __dirname + "/recieved/"
             dirpath += filename.slice(0, filename.lastIndexOf('/'));
-            //console.log(dirpath);
+            console.log(dirpath);
             if (!fs.existsSync(dirpath)) {
                 fs.mkdirSync(dirpath, { recursive: true });
             }
-            process.stdout.write("Saving: " + filename.slice(filename.lastIndexOf('/') + 1,) + '\t');
+            //process.stdout.write("Saving: " + filename.slice(filename.lastIndexOf('/') + 1,) + '\t');
             saved += "<br>" + filename
             const saveTo = path.join(__dirname + "/recieved", filename);
-            file.pipe(fs.createWriteStream(saveTo));
-            process.stdout.write('✅\n');
+            console.log(saveTo);
+            let save = file.pipe(saveTo); // the file is getting piped into fs to be saved
+            var hold = 0;
+            var value = 0; 
+            var progress = setInterval(function () {
+                var wb = save.bytesWritten;
+                if (hold != wb) {
+                    total += save.bytesWritten - hold;
+                    hold = wb;
+                } else {
+                    clearInterval(progress);
+                    hold = 0;
+                }
+                //console.dir("Current file written in KB: " + wb / (1024 ^ 2))
+                //console.dir("Total written in: " + ((total / (1024 ^ 2)) / file_size) * 100);
+                value = Math.ceil((((total / (1024 ^ 2)) / file_size) * 100))
+                bar.update(value);
+                io.emit('progress', { progress: value });
+                /* this is a temporary solution, find a way get the full file size
+                then calculate the percentage and post that to the progress bar on 
+                the client side also make it elegant*/
+            }, 500);
+            //save.addListener("progress", () => { console.log(save.bytesWritten / 1024) })
+            //process.stdout.write('✅\n');
         });
         bb.on('close', () => {
+            bar.update(100);
+            io.emit('progress', { progress: 100 });
+            bar.stop();
             res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
             res.end(`<h3>Upload success:</h3> ${saved}`);
         });
